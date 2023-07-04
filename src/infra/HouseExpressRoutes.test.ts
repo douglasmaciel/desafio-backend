344d335e33aa02a641e56d21710540150906da3e
Douglas Maciel <dmaciel1982@hotmail.com>
2023-07-04 13:24:29 -0300

    implementa rotas da api

---
+++ src/infra/HouseExpressRoutes.test.ts
@@ -0,0 +1,241 @@
+import "dotenv/config";
+import { test, expect, beforeEach } from "vitest";
+
+const port = process.env.PORT || 3005;
+const baseURL = `http://localhost:${port}`;
+const isToSkip = process.env.E2E === undefined;
+
+let headers: Headers;
+beforeEach(() => {
+  headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json; charset=UTF-8",
+  });
+});
+
+test.skipIf(isToSkip)(
+  "[GET /api/houses] Deve retornar uma lista de casas ou uma lista vazia",
+  async () => {
+    const res = await fetch(`${baseURL}/api/houses`, {
+      method: "GET",
+      headers: headers,
+    });
+    const payload = await res.json();
+    expect(payload.data).toBeTypeOf(typeof []);
+  }
+);
+
+test.skipIf(isToSkip)(
+  "[GET /api/houses/:id] Deve cadastrar uma casa e buscar pelo id",
+  async () => {
+    const body = JSON.stringify({
+      name: "house name",
+      region: "house region",
+      foundationDate: "house foundation",
+      lord: {
+        name: "lord of house",
+        seasons: ["season 1"],
+      },
+    });
+    const res = await fetch(`${baseURL}/api/houses`, {
+      method: "POST",
+      body: body,
+      headers: headers,
+    });
+    const payload = await res.json();
+    const { id } = payload.data;
+    const { name, region, foundationDate, currentLord } =
+      payload.data.attributes;
+    expect(id).toBeDefined();
+    expect(name).toBe("house name");
+    expect(region).toBe("house region");
+    expect(foundationDate).toBe("house foundation");
+    expect(currentLord.name).toBe("lord of house");
+    expect(currentLord.seasons).toEqual(["season 1"]);
+    const getRes = await fetch(`${baseURL}/api/houses/${id}`, {
+      method: "GET",
+      headers: headers,
+    });
+    const getPayload = await getRes.json();
+    const { id: getId } = getPayload.data;
+    const {
+      name: getName,
+      region: getRegion,
+      foundationDate: getFoundationDate,
+      currentLord: getCurrentLord,
+    } = getPayload.data.attributes;
+    expect(getId).toBeDefined();
+    expect(getName).toBe("house name");
+    expect(getRegion).toBe("house region");
+    expect(getFoundationDate).toBe("house foundation");
+    expect(getCurrentLord.name).toBe("lord of house");
+    expect(getCurrentLord.seasons).toEqual(["season 1"]);
+  }
+);
+
+test.skipIf(isToSkip)(
+  "[GET /api/houses/query?name=] Deve cadastrar uma casa e buscar pelo nome",
+  async () => {
+    const body = JSON.stringify({
+      name: "house name",
+      region: "house region",
+      foundationDate: "house foundation",
+      lord: {
+        name: "lord of house",
+        seasons: ["season 1"],
+      },
+    });
+    const res = await fetch(`${baseURL}/api/houses`, {
+      method: "POST",
+      body: body,
+      headers: headers,
+    });
+    const payload = await res.json();
+    const { id } = payload.data;
+    const { name, region, foundationDate, currentLord } =
+      payload.data.attributes;
+    expect(id).toBeDefined();
+    expect(name).toBe("house name");
+    expect(region).toBe("house region");
+    expect(foundationDate).toBe("house foundation");
+    expect(currentLord.name).toBe("lord of house");
+    expect(currentLord.seasons).toEqual(["season 1"]);
+    const getRes = await fetch(`${baseURL}/api/houses/query?name=${name}`, {
+      method: "GET",
+      headers: headers,
+    });
+    const getPayload = await getRes.json();
+    const { id: getId } = getPayload.data;
+    const {
+      name: getName,
+      region: getRegion,
+      foundationDate: getFoundationDate,
+      currentLord: getCurrentLord,
+    } = getPayload.data.attributes;
+    expect(getId).toBeDefined();
+    expect(getName).toBe("house name");
+    expect(getRegion).toBe("house region");
+    expect(getFoundationDate).toBe("house foundation");
+    expect(getCurrentLord.name).toBe("lord of house");
+    expect(getCurrentLord.seasons).toEqual(["season 1"]);
+  }
+);
+
+test.skipIf(isToSkip)(
+  "[POST /api/houses] Deve cadastrar uma casa e retornar o que foi cadastrado",
+  async () => {
+    const body = JSON.stringify({
+      name: "house name",
+      region: "house region",
+      foundationDate: "house foundation",
+      lord: {
+        name: "lord of house",
+        seasons: ["season 1"],
+      },
+    });
+    const res = await fetch(`${baseURL}/api/houses`, {
+      method: "POST",
+      body: body,
+      headers: headers,
+    });
+    const payload = await res.json();
+    const { id } = payload.data;
+    const { name, region, foundationDate, currentLord } =
+      payload.data.attributes;
+    expect(id).toBeDefined();
+    expect(name).toBe("house name");
+    expect(region).toBe("house region");
+    expect(foundationDate).toBe("house foundation");
+    expect(currentLord.name).toBe("lord of house");
+    expect(currentLord.seasons).toEqual(["season 1"]);
+  }
+);
+
+test.skipIf(isToSkip)(
+  "[PUT /api/houses/:id] Deve cadastrar e, depois, atualizar o nome de uma casa e retornar o que foi cadastrado",
+  async () => {
+    const body = JSON.stringify({
+      name: "house name",
+      region: "house region",
+      foundationDate: "house foundation",
+      lord: {
+        name: "lord of house",
+        seasons: ["season 1"],
+      },
+    });
+    const res = await fetch(`${baseURL}/api/houses`, {
+      method: "POST",
+      body: body,
+      headers: headers,
+    });
+    const payload = await res.json();
+    const { id } = payload.data;
+    const { name, region, foundationDate, currentLord } =
+      payload.data.attributes;
+    expect(id).toBeDefined();
+    expect(name).toBe("house name");
+    expect(region).toBe("house region");
+    expect(foundationDate).toBe("house foundation");
+    expect(currentLord.name).toBe("lord of house");
+    expect(currentLord.seasons).toEqual(["season 1"]);
+    const putBody = JSON.stringify({
+      name: "new name",
+    });
+    const putRes = await fetch(`${baseURL}/api/houses/${id}`, {
+      method: "PUT",
+      body: putBody,
+      headers: headers,
+    });
+    const putPayload = await putRes.json();
+    const { id: putId } = putPayload.data;
+    const {
+      name: putName,
+      region: putRegion,
+      foundationDate: putFoundationDate,
+      currentLord: putCurrentLord,
+    } = putPayload.data.attributes;
+    expect(putId).toBeDefined();
+    expect(putName).toBe("new name");
+    expect(putRegion).toBe("house region");
+    expect(putFoundationDate).toBe("house foundation");
+    expect(putCurrentLord.name).toBe("lord of house");
+    expect(putCurrentLord.seasons).toEqual(["season 1"]);
+  }
+);
+
+test.skipIf(isToSkip)(
+  "[DELETE /api/houses/:id] Deve cadastrar e, depois, remover uma casa e retornar o que foi removido",
+  async () => {
+    const body = JSON.stringify({
+      name: "house name",
+      region: "house region",
+      foundationDate: "house foundation",
+      lord: {
+        name: "lord of house",
+        seasons: ["season 1"],
+      },
+    });
+    const res = await fetch(`${baseURL}/api/houses`, {
+      method: "POST",
+      body: body,
+      headers: headers,
+    });
+    const payload = await res.json();
+    const { id } = payload.data;
+    const { name, region, foundationDate, currentLord } =
+      payload.data.attributes;
+    expect(id).toBeDefined();
+    expect(name).toBe("house name");
+    expect(region).toBe("house region");
+    expect(foundationDate).toBe("house foundation");
+    expect(currentLord.name).toBe("lord of house");
+    expect(currentLord.seasons).toEqual(["season 1"]);
+    const delRes = await fetch(`${baseURL}/api/houses/${id}`, {
+      method: "DELETE",
+      headers: headers,
+    });
+    const delPayload = await delRes.json();
+    const { id: delId } = delPayload.data;
+    expect(delId).toBe(id);
+  }
+);
--- src/infra/HouseExpressRoutes.ts
+++ src/infra/HouseExpressRoutes.ts
@@ -1,12 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
 import { HouseRepositoryMemory } from "./HouseRepositoryMemory";
-import { houseOutDTO } from "../domain/House";
-import { Name } from "../domain/valueObjects/Name";
-import { HouseId } from "../domain/valueObjects/HouseId";
+import { House, houseOutDTO } from "../domain/House";
 
 const houseExpressRouter = express.Router();
 houseExpressRouter.use(express.json());
 const houseRepository = new HouseRepositoryMemory();
+type houseInputDTO = {
+  name: string;
+  region: string;
+  foundationDate: string;
+  lord?: {
+    name: string;
+    seasons: string[];
+  };
+};
 
 houseExpressRouter.get("/houses", async (req: Request, res: Response) => {
   let output: { data: houseOutDTO[] } | { error: any };
@@ -28,8 +35,7 @@ houseExpressRouter.get("/houses/query", async (req: Request, res: Response) => {
     return;
   }
   try {
-    const qName = req.query.name ?? "";
-    const name = new Name(qName);
+    const name = req.query.name ?? "";
     const house = await houseRepository.getByName(name);
     if (house === undefined) {
       res.sendStatus(404);
@@ -47,13 +53,85 @@ houseExpressRouter.get("/houses/query", async (req: Request, res: Response) => {
 houseExpressRouter.get("/houses/:id", async (req: Request, res: Response) => {
   let output: { data: houseOutDTO } | { error: any };
   try {
-    const id = new HouseId(req.params.id);
+    const id = req.params.id;
     const house = await houseRepository.getById(id);
     if (house === undefined) {
       res.sendStatus(404);
       return;
     }
     output = { data: house.toOutDTO() };
+  } catch (e) {
+    output = { error: e };
+    if (e instanceof Error) output = { error: e.message };
+    res.status(500);
+  }
+  res.json(output);
+});
+
+houseExpressRouter.post("/houses", async (req: Request, res: Response) => {
+  let output: { data: houseOutDTO | {} } | { error: any };
+  try {
+    const { name, region, foundationDate, lord } = req.body as houseInputDTO;
+    if (
+      name === undefined ||
+      region === undefined ||
+      foundationDate === undefined
+    ) {
+      res.sendStatus(400);
+      return;
+    }
+    const houseId = await houseRepository.nextId();
+    const house = new House(houseId, name, region, foundationDate, lord);
+    await houseRepository.save(house);
+    const savedHouse = await houseRepository.getById(houseId);
+    output = { data: savedHouse?.toOutDTO() ?? {} };
+  } catch (e) {
+    output = { error: e };
+    if (e instanceof Error) output = { error: e.message };
+    res.status(500);
+  }
+  res.json(output);
+});
+
+houseExpressRouter.delete(
+  "/houses/:id",
+  async (req: Request, res: Response) => {
+    let output: { data: houseOutDTO } | { error: any };
+    try {
+      const id = req.params.id;
+      const house = await houseRepository.getById(id);
+      if (house === undefined) {
+        res.sendStatus(404);
+        return;
+      }
+      await houseRepository.remove(house);
+      output = { data: house.toOutDTO() };
+    } catch (e) {
+      output = { error: e };
+      if (e instanceof Error) output = { error: e.message };
+      res.status(500);
+    }
+    res.json(output);
+  }
+);
+
+houseExpressRouter.put("/houses/:id", async (req: Request, res: Response) => {
+  let output: { data: houseOutDTO | {} } | { error: any };
+  try {
+    const { name, region, foundationDate, lord } = req.body as houseInputDTO;
+    const id = req.params.id;
+    const house = await houseRepository.getById(id);
+    if (house === undefined) {
+      res.sendStatus(404);
+      return;
+    }
+    house.name = name ?? house.name;
+    house.region = region ?? house.region;
+    house.foundationDate = foundationDate ?? house.foundationDate;
+    if (lord) house.lord = lord;
+    await houseRepository.save(house);
+    const savedHouse = await houseRepository.getById(id);
+    output = { data: savedHouse?.toOutDTO() ?? {} };
   } catch (e) {
     output = { error: e };
     if (e instanceof Error) output = { error: e.message };
